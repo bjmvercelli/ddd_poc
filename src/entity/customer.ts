62bb6b1b6ce4cc138dc00788116cec94bd5246ff
Bruno Vercelli <bruno.vercelli@afya.com.br>
2023-10-19 17:49:59 -0300

    Implement autovalidation concept

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -2,10 +2,31 @@ export class Customer {
   _id: string;
   _name: string;
   _address: string;
+  _active: boolean = false;
 
   constructor(id: string, name: string, address: string) {
     this._id = id;
     this._name = name;
     this._address = address;
+
+    this.validate();
+  }
+
+  validate() {
+    if (!this._name){
+      throw new Error("Name is required");
+    }
+
+    if (!this._id){
+      throw new Error("Id is required");
+    }
+  }
+
+  activate(){
+    if (!this._address){
+      throw new Error("Address is required");
+    }
+
+    return this._active = true;
   }
 }
