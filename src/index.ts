e07b1fd39aac0c37bb57ed65246d3d0ba8fa19e5
Bruno Vercelli <bruno.vercelli@afya.com.br>
2023-10-24 22:01:14 -0300

    Add tsc before tests to check syntax

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const address = new Address("123 Main St", "Anytown", "NY", "12345");
 customer.Address = address;
 
 
-const item1 = new OrderItem("123", "Item 1", 9.99);
-const item2 = new OrderItem("456", "Item 2", 19.99);
+const item1 = new OrderItem("123", "Item 1", 9.99, "123", 1);
+const item2 = new OrderItem("456", "Item 2", 19.99, "456", 2);
 
 const order = new Order("123", "123", [item1, item2]);
